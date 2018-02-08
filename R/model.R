6b1c6480512bad3a3a8266d00775a061da80d376
Jorge Rodriguez-Veiga <jorge.rodriguez.veiga@gmail.com>
2018-02-08 22:50:40 +0100

    Create model class.

--- R/model.R
+++ R/model.R
@@ -1,12 +1,23 @@
 
+# Model -----------------------------------------------------------------------
+Model <- function(){
+  return(
+    ProblemInfoClass(
+      sets=list(),
+      parameters=list(),
+      variables=list(),
+      objectives=list(),
+      constraints=list()
+    )
+  )
+}
+# -----------------------------------------------------------------------------
 
 
-
-
-# Create Model class -----------------------------------------------------------
-Model <- setClass(
+# ModelClass ------------------------------------------------------------------
+ModelClass <- setClass(
   # Class name
-  "Model",
+  "ModelClass",
   
   # Define the slots
   slots = c(
@@ -24,4 +35,35 @@ Model <- setClass(
   }
 )
 # --------------------------------------------------------------------------- #
+
+
+# =============================================================================
+# Operations
+# =============================================================================
+# $ ---------------------------------------------------------------------------
+setMethod(
+  "$", 
+  signature(e1 = "ModelClass", e2 = "ANY"), 
+  function(e1, e2){
+    if(class(e2)=="SetClass"){
+      e1@sets[[e2@name]] <- e2
+    }else if(class(e2)=="ParamClass"){
+      e1@parameters[[e2@name]] <- e2
+    }else if(class(e2)=="ParamElementClass"){
+      e1@parameters[[e2@name]] <- e2
+    }else if(class(e2)=="VarClass"){
+      e1@variables[[e2@name]] <- e2
+    }else if(class(e2)=="VarElementClass"){
+      e1@variables[[e2@name]] <- e2
+    }else if(class(e2)=="ObjectiveClass"){
+      e1@objectives[[e2@name]] <- e2
+    }else if(class(e2)=="ConstraintClass"){
+      e1@constraints[[e2@name]] <- e2
+    }else if(class(e2)=="ConstraintElementClass"){
+      e1@constraints[[e2@name]] <- e2
+    }else{
+      stop("Unknown object class.")
+    }
+  }
+)
 
